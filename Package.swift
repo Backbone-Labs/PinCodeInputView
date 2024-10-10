3b833fb6b53ead32a2e5bafba8f48e48bbd5a3e2
Zhuo Huang <zhuo@playbackbone.com>
2024-10-10 11:51:35 -0700

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "PinCodeInputView",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "PinCodeInputView",
+            targets: ["PinCodeInputView"]
+        ),
+    ],
+    dependencies: [
+        // Add dependencies if necessary
+    ],
+    targets: [
+        .target(
+            name: "PinCodeInputView",
+            path: "PinCodeInputView",  // Update to reflect the correct path to your source files
+            exclude: [],  // Exclude unnecessary files if needed
+            publicHeadersPath: nil  // Set if there are Objective-C headers, otherwise leave as nil
+        ),
+    ],
+    swiftLanguageVersions: [.v5]  // Updated for Swift 5
+)
